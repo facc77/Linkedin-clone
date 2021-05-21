bec51c3f6a618176d613ea1b0443b61bc769a1ef
facc77 <ferrerfacundo96@gmail.com>
2021-05-21 16:11:14 -0300

    adding header

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login.js";
+import Home from "./components/Home";
+import Header from "./components/Header";
 import "./index.css";
 
 function App() {
@@ -10,6 +12,10 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
