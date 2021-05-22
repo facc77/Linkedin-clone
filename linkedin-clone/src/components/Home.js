403fddd77f73af9830cc24cdd2292ea0b1de1eec
facc77 <ferrerfacundo96@gmail.com>
2021-05-22 15:45:27 -0300

    configuring home section

--- linkedin-clone/src/components/Home.js
+++ linkedin-clone/src/components/Home.js
@@ -2,5 +2,74 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Home(props) {
-  return <div>home</div>;
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Contratando ya!</a>
+        </h5>{" "}
+        <p>Encuentra gente talentosa en tiempo record y levanta tu negocio</p>
+      </Section>
+      <Layout>
+        <div>Left side</div>
+        <div>main</div>
+        <div>right side</div>
+      </Layout>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
+
+const Content = styled.div`
+  max-width: 1128px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Section = styled.div`
+  min-height: 50px;
+  padding: 16px 0;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+  p {
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /*   grid-template-row: auto;
+ */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
